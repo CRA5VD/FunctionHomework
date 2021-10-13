808c8ccaa865cd95f873f4d9951825bed205c9fc
Connie <constancecrawford87@gmail.com>
2021-10-13 10:16:08 -0400

    function homework

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 function myFunction(argument1, argument2) {
     console.log(argument1 + argument2);
 }
+myFunction(10,20);
+myFunction(35,20);
 
 function mySubtraction(argument1, argument2) {
     console.log(argument1 - argument2)
 }
+mySubtraction(55, 25);
+mySubtraction(30, 25);
+
 
 function myOtherFunction(argument1, argument2) {
     console.log(argument1 * argument2)
 }
+myOtherFunction(19, 84);
+myOtherFunction(32, 48);
 
 function myLastFunction(argument1, argument2) {
     console.log(argument1 / argument2)
 }
+myLastFunction(81, 9);
+myLastFunction(35, 7);
